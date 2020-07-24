ba7cff8727e8e012316178683ee48ab97f773ebb
devwalex <salamiusmanolawale@gmail.com>
2020-07-24 18:15:35 +0100

    Updates

---
+++ precache-manifest.c4ecb66b97c5987d248f82f07f0f87df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a036d5360e10595006ab069e3cc56106",
+    "url": "/simple-search/index.html"
+  },
+  {
+    "revision": "4429506d09acc6792339",
+    "url": "/simple-search/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "eb98599a440921847ed1",
+    "url": "/simple-search/static/css/main.d2b50e35.chunk.css"
+  },
+  {
+    "revision": "4429506d09acc6792339",
+    "url": "/simple-search/static/js/2.2af23293.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-search/static/js/2.2af23293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb98599a440921847ed1",
+    "url": "/simple-search/static/js/main.eed5f8c5.chunk.js"
+  },
+  {
+    "revision": "89c8369a95919fb5c895",
+    "url": "/simple-search/static/js/runtime-main.4a3cacbc.js"
+  }
+]);
