cc3f16fb479252659901f0e73ff13e2bf4a80a34
devwalex <salamiusmanolawale@gmail.com>
2020-07-24 13:37:36 +0100

    feat: add search box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import MovieList from "./MovieList";
+import SearchBox from "./SearchBox";
 import { movies } from "./movies";
 
 const App = () => {
   return (
     <div className="tc">
+      <h1>Simple Search</h1>
+      <SearchBox />
       <MovieList movies={movies} />
     </div>
   );
---
+++ src/SearchBox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchBox = () => {
+  return (
+    <div className="pa3">
+      <input className="pa3 ba b--blue bg-lightest-blue" type="search" placeholder="Search Movies" />
+    </div>
+  );
+};
+
+export default SearchBox;
